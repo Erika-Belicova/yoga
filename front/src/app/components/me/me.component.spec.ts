3af578f33953e2ef586ef5adff7a2a57e22245a9
Erika Belicova <146355532+Erika-Belicova@users.noreply.github.com>
2025-05-30 20:24:10 +0200

    Refactor component unit tests into integration tests to improve integration test coverage and streamline testing process

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,14 +11,13 @@ import { expect } from '@jest/globals';
 import { MeComponent } from './me.component';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
 import { mockUser } from '../../mocks/user.mocks';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let httpTestingController: HttpTestingController;
 
-  let mockUserService: Partial<UserService>;
   let mockRouter: Partial<Router>;
   let mockSnackBar: Partial<MatSnackBar>;
 
@@ -28,43 +27,46 @@ describe('MeComponent', () => {
       id: 1
     },
     logOut: jest.fn()
-  }
+  };
 
   beforeEach(async () => {
-    mockUserService = {
-      getById: jest.fn().mockReturnValue(of(mockUser)),
-      delete: jest.fn().mockReturnValue(of({}))
-    };
-
     mockRouter = {
-      navigate: jest.fn(),
+      navigate: jest.fn()
     };
-
     mockSnackBar = {
-      open: jest.fn(),
+      open: jest.fn()
     };
 
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
       imports: [
         MatSnackBarModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService },
-                  { provide: UserService, useValue: mockUserService },
-                  { provide: Router, useValue: mockRouter },
-                  { provide: MatSnackBar, useValue: mockSnackBar }
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: Router, useValue: mockRouter },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+        UserService
       ],
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+
+    const req = httpTestingController.expectOne('api/user/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
@@ -73,13 +75,21 @@ describe('MeComponent', () => {
 
   it('should set user on init', () => {
     component.ngOnInit();
+
+    const req = httpTestingController.expectOne('api/user/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
+
     expect(component.user).toEqual(mockUser);
   });
 
   it('should delete account and perform necessary actions', () => {
     component.delete();
 
-    expect(mockUserService.delete).toHaveBeenCalledWith('1');
+    const req = httpTestingController.expectOne('api/user/1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
+
     expect(mockSnackBar.open).toHaveBeenCalledWith("Your account has been deleted !", 'Close', { duration: 3000 });
     expect(mockSessionService.logOut).toHaveBeenCalled();
     expect(mockRouter.navigate).toHaveBeenCalledWith(['/']);
@@ -88,7 +98,7 @@ describe('MeComponent', () => {
   it('should verify that back has been called', () => {
     const windowSpy = jest.spyOn(window.history, 'back');
     component.back();
-    
+
     expect(windowSpy).toHaveBeenCalled();
     windowSpy.mockRestore();
   });
--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -11,27 +11,21 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { mockLoginRequest } from 'src/app/mocks/auth.mocks';
-import { of, throwError } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpTestingController: HttpTestingController;
 
-  let mockAuthService: Partial<AuthService>;
   let mockSessionService: Partial<SessionService>;
   let mockRouter: Partial<Router>;
 
   beforeEach(async () => {
-    mockAuthService = {
-      login: jest.fn().mockReturnValue(of(mockLoginRequest))
-    };
-
     mockSessionService = {
       logIn: jest.fn(),
-    }
+    };
 
     mockRouter = {
       navigate: jest.fn(),
@@ -39,42 +33,52 @@ describe('LoginComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [ { provide: SessionService, useValue: mockSessionService },
-                  { provide: AuthService, useValue: mockAuthService },
-                  { provide: Router, useValue: mockRouter } ],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: Router, useValue: mockRouter }
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         MatCardModule,
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule]
-    })
-      .compileComponents();
+        ReactiveFormsModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should log in and redirect on successful login', () => {
-    const logInSpy = jest.spyOn(mockSessionService, 'logIn');
+  it('should log in and redirect on successful login', (done) => {
     component.form.setValue({ email: 'claire@perret.com', password: 'password' });
     component.submit();
 
-    expect(mockAuthService.login).toHaveBeenCalledWith({ email: 'claire@perret.com', password: 'password' });
-    expect(logInSpy).toHaveBeenCalledWith(mockLoginRequest);
+    const req = httpTestingController.expectOne('api/auth/login');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockLoginRequest);
+
+    expect(mockSessionService.logIn).toHaveBeenCalledWith(mockLoginRequest);
     expect(mockRouter.navigate).toHaveBeenCalledWith(['/sessions']);
+    done();
   });
 
   it('should invalidate an empty login form', () => {
     component.form.setValue({ email: '', password: '' });
-    component.submit();
 
     expect(component.form.invalid).toBe(true);
   });
@@ -88,9 +92,13 @@ describe('LoginComponent', () => {
   });
 
   it('should set onError to true in case of a login error', () => {
-    mockAuthService.login = jest.fn().mockReturnValue(throwError(() => new Error('Login failed')));
+    component.form.setValue({ email: 'claire@perret.com', password: 'password' });
     component.submit();
 
+    const req = httpTestingController.expectOne('api/auth/login');
+    expect(req.request.method).toBe('POST');
+    req.flush('Login failed', { status: 401, statusText: 'Unauthorized' });
+
     expect(component.onError).toBe(true);
-  })
+  });
 });
